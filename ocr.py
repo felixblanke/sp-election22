a74135e1c37771d5a0048ca3cd33201eaed95cdf
Felix Blanke <felixblanke@uni-bonn.de>
2022-01-02 23:45:07 +0100

    Improve cmdline args

--- ocr.py
+++ ocr.py
@@ -4,13 +4,34 @@ from pdf2image import convert_from_path
 
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("first_page", type=int)
-parser.add_argument("--num-pages", type=int, default=4)
+parser = argparse.ArgumentParser(
+    "OCR recognition of pdf pages",
+    description="Transforms pdf pages to Pillow Images using pdf2image and then uses\
+        tesseract for OCR. The recognized text is printed to stdout."
+)
+parser.add_argument("first_page", type=int, help="Number of the first page to consider")
+parser.add_argument(
+    "--path",
+    type=str,
+    default="Wahlzeitung_2022.pdf",
+    help="Path of the pdf file (default: Wahlzeitung_2022.pdf)"
+)
+parser.add_argument(
+    "--num-pages",
+    type=int,
+    default=4,
+    help="Number of pages to consider, starting from first_page (default: 4)"
+)
+parser.add_argument(
+    "--lang",
+    type=str,
+    default="deu",
+    help="three letter language code of the text to recognize (default: deu)"
+)
 args = parser.parse_args()
 
 images = convert_from_path(
-    '/home/felix/git/ocr_wahlzeitungsbeitraege22/Wahlzeitung_2022.pdf',
+    args.path,
     dpi=300,
     thread_count=12,
     first_page=args.first_page,
@@ -19,4 +40,4 @@ images = convert_from_path(
 
 for idx, img in enumerate(images):
     print(f"~~~ PAGE {idx + args.first_page} ~~~")
-    print(pytesseract.image_to_string(img, lang='deu'))
+    print(pytesseract.image_to_string(img, lang=args.lang))
